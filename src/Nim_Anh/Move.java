3e483f68064c5fb36a31a61ee04f7fb9b2cca932
DUT <142745058+ComS-2023@users.noreply.github.com>
2024-04-24 12:57:12 -0500

    Create Move.java

---
+++ src/Nim_Anh/Move.java
@@ -0,0 +1,26 @@
+package Nim_Anh;
+public class Move {
+    private int HIndex;
+    private int numObjectsRemoved;
+
+    public Move(int heapIndex, int numObjectsRemoved) {
+        this.HIndex = heapIndex;
+        this.numObjectsRemoved = numObjectsRemoved;
+    }
+
+    public int getHeapIndex() {
+        return HIndex;
+    }
+
+    public void setHeapIndex(int heapIndex) {
+        this.HIndex = heapIndex;
+    }
+
+    public int getNumObjectsRemoved() {
+        return numObjectsRemoved;
+    }
+
+    public void setNumObjectsRemoved(int numObjectsRemoved) {
+        this.numObjectsRemoved = numObjectsRemoved;
+    }
+}
