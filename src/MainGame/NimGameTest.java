893f13cb7e65489aeebb4a9cd9da7c594bb935a6
DUT <142745058+ComS-2023@users.noreply.github.com>
2024-04-27 01:09:23 +0000

    changes

--- src/MainGame/NimGameTest.java
+++ src/MainGame/NimGameTest.java
@@ -46,10 +46,10 @@ public class NimGameTest {
         // Option 1: New Game
         if (option.equalsIgnoreCase("N")){
             // Start a new game
-            Game maiGame = new Game(leaderboard);
+            Game mainGame = new Game(leaderboard);
 
             // Create players
-            maiGame.createNewPlayers();
+            mainGame.createNewPlayers();
 
             /*
                 This loop is the game
@@ -60,21 +60,33 @@ public class NimGameTest {
                 //Selecting first Player
                 System.out.print("Choose the player for the first turn (1 or 2): ");
                 int firstTurn = scan.nextInt();
-                // if User press Cancel break out of the game
-                if(firstTurn == null){
-                    break;
+
+                // Check user input
+                while (firstTurn != 1 || firstTurn != 2){
+                    System.out.println("Invalid input!");
+                    System.out.println();
+                    System.out.print("Choose the player for the first turn (1 or 2): ");
+                    firstTurn = scan.nextInt();
                 }
-                // Creating the New Game and sending players and First turn
-                Nim newGame = new Nim(players,firstTurn);
-                //winner is returned as a String
-                String winner = newGame.play();
+
+                // Start the game
+                if (firstTurn == 1)
+                    mainGame.startGame(mainGame.getPlayers()[0].getPlayerName());
+                else
+                    mainGame.startGame(mainGame.getPlayers()[1].getPlayerName());
+                
+;
+                // Winner is returned as a String
+                String winner = mainGame.play();
+                
                 //Handeling a cancel
                 if(winner == null){
                     break;
                 }
                 System.out.println("\n\nWinner is: " + winner );
+                
                 // Asking to play again
-                continuePlaying = playAgain();
+                continuePlaying = playAgain(scan);
             }
         }
 
@@ -93,34 +105,25 @@ public class NimGameTest {
         
     }
     
-  
-    
-    /**
-     * 
-     * @param players Array of players playing
-     * @return The name of the player that will play first
-     */
-    public static String selectFirstPlayer(Player[] players){
-        //Options of the Players name
-        String[] playersNames = {players[0].getName(),players[1].getName()};
-        //Getting input for which player is first
-        String firstTurn = (String)JOptionPane.showInputDialog(null,"Select Player Name: ","Playing First",JOptionPane.QUESTION_MESSAGE,null,playersNames,playersNames[0]);
-        return firstTurn; 
-    }
-    
-    /**
-     * Would you like to play Again.
-     * @return True if you want to play again
-     */
-    public static boolean playAgain(){
-        //Getting input from user [YES, NOT]
-        int again = JOptionPane.showConfirmDialog(null, "Play Again","Another Game",JOptionPane.YES_NO_OPTION );
-        //Return true if YES is selected
-        if(again == JOptionPane.YES_OPTION){
-            return true;
-        }else{
-            return false;
+
+    public static boolean playAgain(Scanner scan){
+        // Ask the user
+        System.out.print("Play again? (Y or N): ");
+        String option = scan.next();
+
+        // Check the input
+        while (!option.equalsIgnoreCase("Y") && !option.equalsIgnoreCase("N")){
+            System.out.println("Invalid input!");
+            System.out.println();
+            System.out.print("Play again? (Y or N): ");
+            option = scan.next();
         }
+
+        // Condition to return
+        if (option.equalsIgnoreCase("Y"))
+            return true;
+        else if (option.equalsIgnoreCase("N"))
+            return false;
     
     }
     
